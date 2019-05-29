b2723ab372e78d2736160e88fcf3db68e9fcf5d7
1005 <1005712782@qq.com>
2019-05-29 22:16:00 +0800

    FirstModify

--- src/algorithm.java
+++ src/algorithm.java
@@ -11,6 +11,7 @@ public class algorithm {
     static String ReadPath2 = "D://date/input_2.txt";
     static String WirtePath = "D://date/out.txt";
     static int num = 0;
+    private static final int MAX = Integer.MAX_VALUE;
     static void create_String(int length) throws IOException {
         String val = "";
         Random random = new Random();
@@ -47,19 +48,16 @@ public class algorithm {
         {
             for(int j=1; j<Str2.length; j++)
             {
-                //对应第一个性质
                 if( Str1[i] == Str2[j])
                 {
                     c[i][j] = c[i-1][j-1] + 1;
                     b[i][j] = 1;
                 }
-                //对应第二或者第三个性质
                 else if(c[i-1][j] >= c[i][j-1])
                 {
                     c[i][j] = c[i-1][j];
                     b[i][j] = 0;
                 }
-                //对应第二或者第三个性质
                 else
                 {
                     c[i][j] = c[i][j-1];
@@ -89,7 +87,42 @@ public class algorithm {
             Display(b, x, i, j-1);
         }
     }
+    static public int[][] FindMaxArray(int[] p){
+        int n = p.length-1;
+        int[][] m = new int[n][n];
+        int[][] s = new int[n][n];
 
+        for(int i = 1;i<n;i++){
+            m[i][i] = 0;
+        }
+        for(int l=2;l<=n;l++){
+            for(int i =1;i < n-l+1;i++){
+                int j = i+l-1;
+                m[i][j] = m[i+1][j]+p[i-1]*p[j]*p[i];
+                s[i][j] = i;
+                for(int k = i;k<j-1;k++){
+                    //p[i-1]*p[k]*p[j];此处有错误
+                    int q = m[i][k]+m[k+1][j]+p[i-1]*p[k]*p[j];
+                    if(q<m[i][j]){
+                        m[i][j]=q;
+                        s[i][j]=k;
+                    }
+                }
+            }
+        }
+        return s;
+    }
+    static void show(int[][] s,int start,int end){
+        if(start == end){
+            System.out.print("A"+(start+1));
+        }
+        else {
+            System.out.print("(");
+            show(s,start,s[start][end]);
+            show(s,s[start][end]+1,end);
+            System.out.print(")");
+        }
+    }
     public static void main(String[] args) throws IOException {
 //        for(int i = 0;i < 2;i++){
 //            create_String(22);
@@ -106,24 +139,27 @@ public class algorithm {
         int length = arrayList.size();
         String[] arraystring = new String[length];
 
-        for(int i = 0;i<length;i++){
+//        for(int i = 0;i<length;i++){
 //            String s = arrayList.get(i);
 //            arraystring[i] = s;
-            arraystring[i] = arrayList.get(i);
-        }
-        String[] s1 = new String[arraystring[0].length()];
-        String[] s2 = new String[arraystring[1].length()];
-        String[] s3 = {" ","A","B","C","B","D","A"};
-        String[] s4 = {" ","B","D","C","A","B","A"};
-        for(int i = 0;i < arraystring[0].length();i++){
-            s1[i] = arraystring[0].substring(i,i+1);
-        }
-        for(int i = 0;i < arraystring[1].length();i++){
-            s2[i] = arraystring[1].substring(i,i+1);
-        }
-        int[][] b = FindMaxStrngArray(s3,s4);
-        Display(b,s3,s3.length - 1,s4.length - 1);
-        System.out.println("长度为"+num);
+//            arraystring[i] = arrayList.get(i);
+//        }
+//        String[] s1 = new String[arraystring[0].length()];
+//        String[] s2 = new String[arraystring[1].length()];
+//        String[] s3 = {" ","A","B","C","B","D","A"};
+//        String[] s4 = {" ","B","D","C","A","B","A"};
+        int[] s5 = {5,10,3,12,5,50,6};
+//        for(int i = 0;i < arraystring[0].length();i++){
+//            s1[i] = arraystring[0].substring(i,i+1);
+//        }
+//        for(int i = 0;i < arraystring[1].length();i++){
+//            s2[i] = arraystring[1].substring(i,i+1);
+//        }
+//        int[][] b = FindMaxStrngArray(s3,s4);
+        int[][] s= FindMaxArray(s5);
+        show(s,0,s5.length-2);
+//        Display(b,s3,s3.length - 1,s4.length - 1);
+//        System.out.println("长度为"+num);
 //        char[] s1 = arraystring[0].toCharArray();
 //        char[] s2 = arraystring[1].toCharArray();
 //        System.out.println(s1);
