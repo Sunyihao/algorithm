1569aeb9141ce814030ceb65e92e5df11a86c8ed
WeDream <1005712782@qq.com>
2019-11-20 09:20:28 +0800

    Data_Structure_Source

--- src/algorithm.java
+++ src/algorithm.java
@@ -1,7 +1,8 @@
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Random;
-
+import java.util.Scanner;
+import java.util.Arrays;
 import static java.lang.reflect.Array.getLength;
 
 
@@ -112,6 +113,88 @@ public class algorithm {
         }
         return s;
     }
+    static class th{
+        int weight;
+        int value;
+        int no;
+    }
+    static void Pack(){
+        System.out.println("输入物品种数");
+        Scanner in = new Scanner(System.in);
+        int type_no = in.nextInt();
+        th[] t = new th[type_no];
+        for(int i = 0;i < type_no;i++){
+            t[i].no = t[i].value = t[i].weight = 9999;
+        }
+        System.out.println("请输入物品重量和价值");
+        for(int i = 0;i < type_no;i++){
+            t[i].weight = in.nextInt();
+            t[i].value = in.nextInt();
+            t[i].no = i + 1;
+        }
+        System.out.println("输入背包容量");
+        int Pack_Capacity = in.nextInt();
+        Arrays.sort(t,0,type_no);
+        int ch;
+        int[] br = new int[100];
+        int[] wr = new int[100];
+        int brr = 0;
+        int wrr = 0;
+        int Pack_Max_value = 0;
+        for(ch = 0;ch < type_no;ch++){
+            if(t[ch].weight > Pack_Capacity){
+                break;
+            }
+            br[brr++] = t[ch].no;
+            wr[wrr++] = t[ch].value;
+            Pack_Max_value = Pack_Max_value + t[ch].value;
+            Pack_Capacity = Pack_Capacity - t[ch].weight;
+
+            if(ch < type_no){
+                br[brr++] = t[ch].no;
+                wr[wrr++] = Pack_Capacity;
+                Pack_Max_value = t[ch].value/t[ch].weight*Pack_Capacity;
+                Pack_Capacity = 0;
+            }
+        }
+    }
+    public static class act{
+        int no = 0;
+        int start = 0;
+        int end = 0;
+    }
+    static boolean cmp(act a,act b){
+        return a.end<= b.end;
+    }
+    static void Activity_A(){
+        int[] r = new int[100];
+        int rr = 0;
+        Scanner in=new Scanner(System.in);
+        System.out.println("请输入活动数");
+        int a;
+        a = in.nextInt();
+        act[] ac = new act[a];
+        for(int i = 0;i<a;i++){
+            ac[i] = new act();
+            ac[i].start = in.nextInt();
+            ac[i].end = in.nextInt();
+            ac[i].no = i + 1;
+
+        }
+//        Arrays.sort(ac,1,a-1);
+        r[rr++] = ac[0].no;
+        int k = 0;
+        for(int i = 0;i<a;i++){
+            if(ac[i].start>=ac[k].end){
+                r[rr++] = ac[i].no;
+                k = i;
+            }
+        }
+
+        for(int i = 0;i<a;i++){
+            System.out.println(r[i]);
+        }
+    }
     static void show(int[][] s,int start,int end){
         if(start == end){
             System.out.print("A"+(start+1));
@@ -148,7 +231,7 @@ public class algorithm {
 //        String[] s2 = new String[arraystring[1].length()];
 //        String[] s3 = {" ","A","B","C","B","D","A"};
 //        String[] s4 = {" ","B","D","C","A","B","A"};
-        int[] s5 = {5,10,3,12,5,50,6};
+//        int[] s5 = {5,10,3,12,5,50,6};
 //        for(int i = 0;i < arraystring[0].length();i++){
 //            s1[i] = arraystring[0].substring(i,i+1);
 //        }
@@ -156,8 +239,8 @@ public class algorithm {
 //            s2[i] = arraystring[1].substring(i,i+1);
 //        }
 //        int[][] b = FindMaxStrngArray(s3,s4);
-        int[][] s= FindMaxArray(s5);
-        show(s,0,s5.length-2);
+//        int[][] s= FindMaxArray(s5);
+//        show(s,0,s5.length-2);
 //        Display(b,s3,s3.length - 1,s4.length - 1);
 //        System.out.println("长度为"+num);
 //        char[] s1 = arraystring[0].toCharArray();
@@ -166,6 +249,7 @@ public class algorithm {
 //        System.out.println(s2);
 //        for(int i = 0;i<s.length;i++){
 //            System.out.println(s[i]);
+        Activity_A();
 //        }
     }
 }
